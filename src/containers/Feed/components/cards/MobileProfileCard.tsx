a6515b9945ad1e2e88cc8e8f5333a9a909772844
tkpepper15 <97776629+tkpepper15@users.noreply.github.com>
2023-06-10 22:59:47 -0400

    Update MobileProfileCard.tsx

--- src/containers/Feed/components/cards/MobileProfileCard.tsx
+++ src/containers/Feed/components/cards/MobileProfileCard.tsx
@@ -9,7 +9,7 @@ type Props = {
 const MobileProfileCard: React.FC<Props> = () => {
   return (
     <div className="block lg:hidden">
-      <div className="p-1 mb-3 dark:text-white">💻 Profile</div>
+      <div className="p-1 mb-3 dark:text-white">😃 Profile</div>
       <div className="p-2 rounded-2xl bg-white dark:bg-zinc-700 mb-4">
         <div className="flex items-center gap-2">
           <Image
@@ -29,48 +29,7 @@ const MobileProfileCard: React.FC<Props> = () => {
             <div className="text-sm">{CONFIG.profile.bio}</div>
           </div>
         </div>
-        {/* <div className="flex">
-          {CONFIG.profile.github && (
-            <a
-              href={`https://github.com/${CONFIG.profile.github}`}
-              rel="noreferrer"
-              target="_blank"
-              className="p-3 dark:hover:bg-zinc-700 rounded-2xl cursor-pointer text-gray-500 dark:text-white "
-            >
-              <AiOutlineGithub className="text-2xl" />
-            </a>
-          )}
-          {CONFIG.profile.instagram && (
-            <a
-              href={`https://www.instagram.com/${CONFIG.profile.instagram}`}
-              rel="noreferrer"
-              target="_blank"
-              className="p-3 dark:hover:bg-zinc-700 rounded-2xl cursor-pointer text-gray-500 dark:text-white"
-            >
-              <AiOutlineInstagram className="text-2xl" />
-            </a>
-          )}
-          {CONFIG.profile.email && (
-            <a
-              href={`mailto:${CONFIG.profile.email}`}
-              rel="noreferrer"
-              target="_blank"
-              className="  overflow-hidden p-3 dark:hover:bg-zinc-700 rounded-2xl cursor-pointer text-gray-500 dark:text-white"
-            >
-              <AiOutlineMail className="text-2xl flex-shrink-0" />
-            </a>
-          )}
-          {CONFIG.profile.linkedin && (
-            <a
-              href={`https://www.linkedin.com/in/${CONFIG.profile.linkedin}`}
-              rel="noreferrer"
-              target="_blank"
-              className="  overflow-hidden p-3 dark:hover:bg-zinc-700 rounded-2xl cursor-pointer text-gray-500 dark:text-white"
-            >
-              <AiFillLinkedin className="text-2xl flex-shrink-0" />
-            </a>
-          )}
-        </div> */}
+        
       </div>
     </div>
   )
